b53b54addc3975a6a519535d24bbae91e4192283
paritoshparashar <parasharparitosh2003@gmail.com>
2023-07-30 22:09:07 +0000

    Slider is working correctly.

--- script.js
+++ script.js
@@ -1,25 +1,73 @@
+// for (let i = 0; i < 78; i++) {
+//     const hsBox = document.createElement('div');
+//     hsBox.id = i + '' ;
+//     hsBox.className = 'hsBox';
+//     hSquare.appendChild(hsBox);
+// }
+// ----------------------
+// const hSquare = document.getElementById('h-square');
+
 const bigBox = document.getElementById('large-square');
+const slider = document.getElementById('slider');
+const sliderValueElement = document.getElementById('sliderValue');
 
 for (let i = 0; i < 256; i++) {
     const smallBox = document.createElement('div');
     smallBox.id = i + '' ;
     smallBox.className = 'smallBox';
     bigBox.appendChild(smallBox);
+}   
+colorDivs();
+// ----------------------Calling this function won't work, if you don't first remove the already occupied divs and then add new ones/
+
+slider.addEventListener('input' , ()=>{
+    const sliderValue = slider.value;
+    sliderValueElement.textContent = sliderValue + 'x' + sliderValue;
+    const totalNumberOfSquares = sliderValue*sliderValue;
+    changeGrid(totalNumberOfSquares , sliderValue);
+});
+
+// function removeExisting (className) {
+//     const divsToRemove = document.querySelectorAll('.' + className);
+//     divsToRemove.forEach( div => {
+//         div.remove();
+//     });
+// }
+
+function changeGrid ( num , root){
+
+    bigBox.innerHTML = '';
+    // removeExisting('smallBox');
+
+    for (let i = 0; i < num; i++) {
+    const smallBox = document.createElement('div');
+    smallBox.id = i + '' ;
+    smallBox.className = 'smallBox';
+    bigBox.appendChild(smallBox);
+    
+    }
+
+    const newHeight =  100 / root;
+    sizeDivs(newHeight);
+    colorDivs();
 }
 
-const hSquare = document.getElementById('h-square');
-
-for (let i = 0; i < 78; i++) {
-    const hsBox = document.createElement('div');
-    hsBox.id = i + '' ;
-    hsBox.className = 'hsBox';
-    hSquare.appendChild(hsBox);
+function sizeDivs ( size ){
+    const divs = document.querySelectorAll('.smallBox');
+    divs.forEach(div => {
+        div.style.width = size+ '%';
+    });
 }
 
-const hoveredDiv = document.querySelectorAll('.smallBox');
 
-hoveredDiv.forEach( hDiv => {
-    hDiv.addEventListener('mouseenter', () => {
-        hDiv.style.backgroundColor = 'red';
+function colorDivs (){
+    const hoveredDiv = document.querySelectorAll('.smallBox');
+
+hoveredDiv.forEach( hoverDiv => {
+    hoverDiv.addEventListener('mouseenter', () => {
+        hoverDiv.style.backgroundColor = 'red';
     });
 });
+}
+
+
